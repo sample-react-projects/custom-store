4844330a613b0f3011d0ae76c7138a279a231b47
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-15 01:46:29 +0530

    Create slices

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App.tsx";
 import "./index.scss";
+import { createAuthenticationSlice } from "./store/authentication.ts";
+import { createCounterSlice } from "./store/counter.ts";
+
+createAuthenticationSlice();
+createCounterSlice();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
