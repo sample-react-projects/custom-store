380820a44f1f27657cee85bce4a24e0bdd9654a5
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-14 21:09:22 +0530

    Declare createAction method

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,3 @@
-import { configureStore } from "@reduxjs/toolkit";
 import { authenticationReducers } from "./authentication";
 import { counterReducers } from "./counter";
 
@@ -10,6 +9,17 @@ export type Store = {
     counter: number;
   };
 };
+
+export type Action<T> = {
+  type: string;
+  payload: Record<keyof T, any>;
+};
+
+export function createSlice<T>(
+  name: string,
+  initialState: T,
+  reducers: Record<string, (state: T, action?: Action<T>) => T>
+) {}
 
 export const store = configureStore<Store>({
   reducer: { authentication: authenticationReducers, counter: counterReducers },
