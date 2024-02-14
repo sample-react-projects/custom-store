cd319a84fcd751737679d79f0365c82c7a607618
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-14 23:14:20 +0530

    Fix counter types

--- src/store/counter.ts
+++ src/store/counter.ts
@@ -1,18 +1,26 @@
 import { createSlice } from ".";
 
-const INITIAL_COUNTER_STATE = {
+type CounterState = {
+  counter: number;
+};
+
+const INITIAL_COUNTER_STATE: CounterState = {
   counter: 0,
+};
+
+const counterMutators = {
+  increment: (state: CounterState) => {
+    return { counter: (state.counter += 1) };
+  },
+  decrement: (state: CounterState) => {
+    return { counter: (state.counter -= 1) };
+  },
 };
 
 export const counterSlice = createSlice<typeof INITIAL_COUNTER_STATE>(
   "counter",
   INITIAL_COUNTER_STATE,
-  {
-    increment: (state) => {
-      return { counter: (state.counter += 1) };
-    },
-    decrement: (state) => {
-      return { counter: (state.counter -= 1) };
-    },
-  }
+  counterMutators
 );
+
+export type counterActions = keyof typeof counterMutators;
