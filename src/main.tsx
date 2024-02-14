98c63c11bb1ab82418e6398a06957b8f3090846b
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-02-14 17:50:41 +0530

    Remove store injection

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App.tsx";
 import "./index.scss";
-import { Provider } from "react-redux";
-import { store } from "./store/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
